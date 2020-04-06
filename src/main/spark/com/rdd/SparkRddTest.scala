365e491c35a0f17f4791f65496d1b4bbb5cd3865
cathabidyiitane <abhl35uhx8797@126.com>
2020-04-06 14:42:59 +0800

    新增scala代码

--- src/main/spark/com/rdd/SparkRddTest.scala
+++ src/main/spark/com/rdd/SparkRddTest.scala
@@ -2,8 +2,21 @@ package com.rdd
 
 /**
   * Created by gouyangyang on 2018/3/16/016.
+  * http://blog.csdn.net/pzw_0612/article/details/47700593
   */
 object SparkRddTest {
+  def main(args: Array[String]) {
 
+    val data = 1 to 20000
 
+    val currntTime = System.currentTimeMillis()
+    increase(data.toList)
+    println("used time=" + (System.currentTimeMillis() - currntTime))
+
+  }
+
+  def increase(list: List[Int]): List[Int] = list match {
+    case List() => List()
+    case head2 :: tail => (head2 + 1) :: increase(tail)
+  }
 }
