516a423188c8929fe936608c318fb5f55d32e4e9
cathabidyiitane <abhl35uhx8797@126.com>
2020-04-11 15:39:59 +0800

    测试

---
+++ src/main/scala/Test2.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by gouyangyang on 2018/3/21/021.
+  */
+object Test2 {
+
+  def main(args: Array[String]): Unit = {
+    val dim_info_spliter = "\\u0001"
+//    val dim_info_spliter = "\001"
+    println(dim_info_spliter)
+
+
+  }
+}
