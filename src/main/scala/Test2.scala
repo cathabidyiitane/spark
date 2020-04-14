0d8bf3b8032acf18bbb30f4c37ec9705d22c00b7
cathabidyiitane <abhl35uhx8797@126.com>
2020-04-14 23:09:18 +0800

    测试

--- src/main/scala/Test2.scala
+++ src/main/scala/Test2.scala
@@ -2,12 +2,17 @@
   * Created by gouyangyang on 2018/3/21/021.
   */
 object Test2 {
-
   def main(args: Array[String]): Unit = {
-    val dim_info_spliter = "\\u0001"
-//    val dim_info_spliter = "\001"
-    println(dim_info_spliter)
-
-
+    //    val dim_info_spliter = "\\u0001"
+    //    val dim_info_spliter = "\001"
+    //    println(dim_info_spliter)
+    val a = "Hello world".filter(_ != 'H').map(_.toUpper)
+    print(a)
   }
+
+
+//  def main(args: Array[String]): Unit = {
+//    val bro_path= args(0)
+//    print(bro_path)
+//  }
 }
