e14b36c0f59cf97a942bf5d299cdd61f9ae03d8b
cathabidyiitane <abhl35uhx8797@126.com>
2020-06-16 13:55:30 +0800

    测试

---
+++ src/main/bf/demo003/Demo003.scala
@@ -0,0 +1,12 @@
+package demo003
+/**
+  * Created by gouyangyang on 2018/5/21/021.
+  */
+object Demo003 {
+  def main(args: Array[String]): Unit = {
+    val name1,name2:String ="100";
+    val name3=100;
+    println(name1,name2)
+    println(name3)
+  }
+}
