925dd9f092037b963103c85ac640533cab1699c1
cathabidyiitane <abhl35uhx8797@126.com>
2020-09-04 09:26:35 +0800

    测试

---
+++ src/main/spark/com/rdd/UdfChangeDateFormat.java
@@ -0,0 +1,14 @@
+package com.rdd;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+/**
+ * Created by gouyangyang on 2018/6/10/010.
+ */
+public class UdfChangeDateFormat extends UDF {
+
+    public static void main(String[] args) throws  Exception {
+
+
+    }
+}
