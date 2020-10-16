bd3725294bd74c3439fb3ec7ed63d9c36dd57401
cathabidyiitane <abhl35uhx8797@126.com>
2020-10-16 16:38:17 +0800

    测试 eleven

--- src/main/wjl/eleven/MainTest.scala
+++ src/main/wjl/eleven/MainTest.scala
@@ -3,7 +3,6 @@ package eleven
 /**
   * @author gouyangyang on 2018/9/25/025 13:19
   */
-
 class ClassApplyTest { //类的apply方法调用示例
   def apply(param: String): String = {
     println("apply method called,the paramter is " + param);
@@ -16,6 +15,4 @@ object MainTest {
   def main(args: Array[String]): Unit = {
 
   }
-
-
 }
