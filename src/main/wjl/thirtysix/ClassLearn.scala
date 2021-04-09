80a245b33113c26203dc2e5b90ae4f3ae1f70109
cathabidyiitane <abhl35uhx8797@126.com>
2021-04-09 09:59:40 +0800

    测试

---
+++ src/main/wjl/thirtysix/ClassLearn.scala
@@ -0,0 +1,35 @@
+package thirtysix
+
+/**
+  * @author gouyangyang on 2019/3/19/019 09:29
+  */
+object ClassLearn {
+
+//
+//  def main(args: Array[String]): Unit = {
+//    val helloWord = new HelloWord()
+//    helloWord.name = "l4" //setter方法 ，如果类中的filed类型是val，则该语句报错
+//    helloWord.hello()
+//    println(helloWord.name) //getter方法
+//  }
+//}
+//
+//class HelloWord {
+//  /**
+//    * 1.如果该field是var类型的变量，并且没有被修饰，那么该类就会默认提供public的getter和setter方法，
+//    * 2.如果该field是var类型的变量，被private修饰，那么该类会自动生成默认的private的getter和setter方法
+//    * 3.如果该field是val类型的，只会生成默认的getter方法
+//    * 4.如果不想生成getter和setter方法则将该field声明成private
+//    * 5.如果想要field只能在本类中访问，不想被其他对象访问到，就用private[this]去修饰
+//    */
+//  var name = "z3"
+//
+//  def hello(): Unit = {
+//    println("hello" + name)
+//  }
+//
+//  def getName: String = name //自定义的getter方法
+//  def name_=(name: String): Unit = { //自定义setter方法，
+//    this.name = name
+//  }
+}
